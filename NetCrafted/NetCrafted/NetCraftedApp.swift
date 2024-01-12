256e0e7bd6c02267f15e23d6140d80d3f0182827
v.kavalenka <v.kavalenka@andersenlab.com>
2024-01-12 22:35:02 +0400

    Add project.

---
+++ NetCrafted/NetCrafted/NetCraftedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetCraftedApp.swift
+//  NetCrafted
+//
+//  Created by Виталий Орехов on 12.01.24.
+//
+
+import SwiftUI
+
+@main
+struct NetCraftedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
